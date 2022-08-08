ab1be2265a493079ff55bea601cf9ac710da9f72
Christopher Lackner <clackner@cerbsim.com>
2022-08-08 17:07:05 +0200

    geometry step

--- meshing_app/mesher.py
+++ meshing_app/mesher.py
@@ -18,8 +18,31 @@ def preprocessGeometry():
     from netgen.occ import OCCGeometry
     from netgen.webgui import Draw
     geo = OCCGeometry("original.step")
-    s = Draw(geo.shape)
+    shape = geo.shape
+    s = Draw(shape)
     json.dump(s.GetData(), open("render_data.json", "w"))
+
+@register_routine
+def runGeometry(geo_file, exterior=None, diam=None):
+    from netgen.occ import OCCGeometry, Glue, Box, Sphere
+    from netgen.webgui import Draw
+    print("run geometry with ", geo_file, exterior, diam)
+    geo = OCCGeometry(geo_file)
+    shape = geo.shape
+    if exterior is not None:
+        bb = shape.bounding_box
+        d = bb[1] - bb[0]
+        if exterior == "box":
+            ext = Box(bb[0] - (diam-1)/2 * d, bb[1] + (diam-1)/2 * d)
+        elif exterior == "sphere":
+            ext = Sphere(bb[0] + 0.5 * d, d.Norm() * diam/2)
+        else:
+            raise Exception(f"Exterior {exterior} unknown")
+        ext -= shape
+        shape = Glue([shape, ext])
+    s = Draw(shape)
+    json.dump(s.GetData(), open("geo_render_data.json", "w"))
+    shape.WriteStep("geo.step")
 
 @register_filetype
 class GeometryFile(SpecialFile):
@@ -33,6 +56,9 @@ class GeometryFile(SpecialFile):
             return json.load(open(self.path("render_data.json")))
         return None
 
+    def get_file_name(self):
+        return self.path("original.step")
+
     @staticmethod
     async def upload(dir_, file):
         os.makedirs(dir_, exist_ok=True)
@@ -41,9 +67,11 @@ class GeometryFile(SpecialFile):
         runRoutine(preprocessGeometry, cwd=geofile.path())
         
 class GeometryUpload(ParameterStep):
-    def __init__(self, name="Geometry Upload"):
+    def __init__(self, path, name="Geometry Upload"):
         super().__init__(name=name)
         self.name = name
+        self.path = path
+        print("model path =", self.path)
         self.geo_file = FileParameter(filetype="mesher_geo",
                                       name="STEP-File",
                                       file_endings=[".step"])
@@ -61,15 +89,39 @@ class GeometryUpload(ParameterStep):
                                                     [self.ext_box],
                                                     [self.ext_sphere]],
                                         selected=[True, False, False])
-
         self.parameters = [self.geo_file, self.exterior_domain]
+        self.last_data = self.get_data()["data"]
 
+    @property
+    def render_data(self):
+        if os.path.exists(self.path("geo_render_data.json")):
+            return json.load(open(self.path("geo_render_data.json")))
+        return None
+
+    def update(self, data, *args, **kwargs):
+        super().update(data, *args, **kwargs)
+        dat = self.get_data()["data"]
+        print("dat =", dat)
+        print("last dat = ", self.last_data)
+        if dat != self.last_data and not self._initial_update:
+            print("changed data")
+            exterior = None
+            diam = 3
+            if self.exterior_domain.selected[1]:
+                exterior = "box"
+                diam = self.ext_box.value
+            elif self.exterior_domain.selected[2]:
+                exterior = "sphere"
+                diam = self.ext_sphere.value
+            runRoutine(runGeometry, cwd=self.path(),
+                       geo_file=self.geo_file.get_file().get_file_name(),
+                       exterior=exterior, diam=diam)
+        self.last_data = dat
 
 class GeometryStep(Step):
-    def __init__(self, geo_file_id=None, name="Geometry"):
+    def __init__(self, upload_step, name="Geometry"):
         self.name = name
-        self.geo_file_id = geo_file_id
-        self.render_data = None
+        self.upload_step = upload_step
         self.materials = []
         self.boundaries = []
         self.add_enclosing_domain = None
@@ -83,8 +135,7 @@ class GeometryStep(Step):
     def get_data(self):
         return { "type" : "Geometry",
                  "name" : self.name,
-                 "data" : { "geo_file_id" : self.geo_file_id,
-                            "render_data" : self.render_data,
+                 "data" : { "render_data" : self.upload_step.render_data,
                             "add_enclosing_domain" : self.add_enclosing_domain } }
     
 
@@ -96,13 +147,8 @@ class MeshingModel(BaseModel):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.geo_upload = GeometryUpload()
-        geo_upload_update = self.geo_upload.update
-        self.geo_step = GeometryStep()
-        def update_render_data(*args, **kwargs):
-            geo_upload_update(*args, **kwargs)
-            self.geo_step.render_data = self.geo_upload.geo_file.get_file().getRenderData()
-        self.geo_upload.update = update_render_data
+        self.geo_upload = GeometryUpload(self.model_file.path)
+        self.geo_step = GeometryStep(self.geo_upload)
         self.steps = [self.geo_upload, self.geo_step]
 
     def run(self, result_dir):
