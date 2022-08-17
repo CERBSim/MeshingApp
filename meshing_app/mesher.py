878940357637e07c17c0e98ee576978c26946193
Christopher Lackner <clackner@cerbsim.com>
2022-08-17 16:47:17 +0200

    first version meshing app working

--- meshing_app/mesher.py
+++ meshing_app/mesher.py
@@ -1,34 +1,26 @@
 
-from webapp.applications.base import BaseModel
+from webapp.applications.base import BaseModel, DrawPNG
 from webapp.applications.parameters import *
 from webapp.applications.steps import *
 from webapp.applications import register_application
 from .version import __version__
 from webapp.filetype import register_filetype, SpecialFile, save_upload_file
 from webapp.routines import runRoutine, register_routine
+from webapp.utils import time_now
 
 import json
-
-from netgen.occ import OCCGeometry
+import os
 
 # TODO: Needs concept of "live resource" that uses available floating computing power for meshing,...
 
 @register_routine
-def preprocessGeometry():
-    from netgen.occ import OCCGeometry
-    from netgen.webgui import Draw
-    geo = OCCGeometry("original.step")
-    shape = geo.shape
-    s = Draw(shape)
-    json.dump(s.GetData(), open("render_data.json", "w"))
-
-@register_routine
-def runGeometry(geo_file, exterior=None, diam=None):
+def runGeometry(geo_file, exterior, diam, glue_solids, nr=0):
     from netgen.occ import OCCGeometry, Glue, Box, Sphere
     from netgen.webgui import Draw
-    print("run geometry with ", geo_file, exterior, diam)
     geo = OCCGeometry(geo_file)
     shape = geo.shape
+    if glue_solids:
+        shape = Glue(shape.solids)
     if exterior is not None:
         bb = shape.bounding_box
         d = bb[1] - bb[0]
@@ -41,7 +33,7 @@ def runGeometry(geo_file, exterior=None, diam=None):
         ext -= shape
         shape = Glue([shape, ext])
     s = Draw(shape)
-    json.dump(s.GetData(), open("geo_render_data.json", "w"))
+    json.dump(s.GetData(), open(f"render_data_{nr}.json", "w"))
     shape.WriteStep("geo.step")
 
 @register_filetype
@@ -51,11 +43,6 @@ class GeometryFile(SpecialFile):
         super().__init__(*args, **kwargs)
         self.file_type = "mesher_geo"
 
-    def getRenderData(self):
-        if os.path.exists(self.path("render_data.json")):
-            return json.load(open(self.path("render_data.json")))
-        return None
-
     def get_file_name(self):
         return self.path("original.step")
 
@@ -64,23 +51,22 @@ class GeometryFile(SpecialFile):
         os.makedirs(dir_, exist_ok=True)
         geofile = GeometryFile(path=dir_)
         save_upload_file(file, geofile.path("original.step"))
-        runRoutine(preprocessGeometry, cwd=geofile.path())
-        
+
 class GeometryUpload(ParameterStep):
     def __init__(self, path, name="Geometry Upload"):
         super().__init__(name=name)
         self.name = name
         self.path = path
-        print("model path =", self.path)
         self.geo_file = FileParameter(filetype="mesher_geo",
                                       name="STEP-File",
                                       file_endings=[".step"])
+        self.geo_file.emit_signal_on_change = "set_renderdata_nr"
 
         self.ext_box = FloatParameter("Diameter (times geo size)",
                                       default=3)
         self.ext_sphere = FloatParameter("Diameter (times geo size)",
                                       default=3)
-        
+
         self.exterior_domain = Selector("Add exterior domain",
                                         options=["None",
                                                  "Box",
@@ -89,22 +75,13 @@ class GeometryUpload(ParameterStep):
                                                     [self.ext_box],
                                                     [self.ext_sphere]],
                                         selected=[True, False, False])
-        self.parameters = [self.geo_file, self.exterior_domain]
+        self.glue_solids = Switch("Glue solids", default=False)
+        self.parameters = [self.geo_file, self.glue_solids, self.exterior_domain]
         self.last_data = self.get_data()["data"]
 
-    @property
-    def render_data(self):
-        if os.path.exists(self.path("geo_render_data.json")):
-            return json.load(open(self.path("geo_render_data.json")))
-        return None
-
     def update(self, data, *args, **kwargs):
-        super().update(data, *args, **kwargs)
         dat = self.get_data()["data"]
-        print("dat =", dat)
-        print("last dat = ", self.last_data)
         if dat != self.last_data and not self._initial_update:
-            print("changed data")
             exterior = None
             diam = 3
             if self.exterior_domain.selected[1]:
@@ -115,7 +92,8 @@ class GeometryUpload(ParameterStep):
                 diam = self.ext_sphere.value
             runRoutine(runGeometry, cwd=self.path(),
                        geo_file=self.geo_file.get_file().get_file_name(),
-                       exterior=exterior, diam=diam)
+                       exterior=exterior, diam=diam, glue_solids=self.glue_solids.value)
+        super().update(data, *args, **kwargs)
         self.last_data = dat
 
 class GeometryStep(Step):
@@ -124,20 +102,29 @@ class GeometryStep(Step):
         self.upload_step = upload_step
         self.materials = []
         self.boundaries = []
-        self.add_enclosing_domain = None
+        self.meshsize_solids = []
+        self.meshsize_faces = []
+        self._render_data = None
 
     def update(self, data, db=None, **kwargs):
-        pass
-
-    def get_geometry(self):
-        pass
+        data = data["data"]
+        if "solid_names" in data:
+            self.materials = data["solid_names"]
+        if "names" in data:
+            self.boundaries = data["names"]
+        if "meshsize_solids" in data:
+            self.meshsize_solids = data["meshsize_solids"]
+        if "meshsize_faces" in data:
+            self.meshsize_faces = data["meshsize_faces"]
 
     def get_data(self):
         return { "type" : "Geometry",
                  "name" : self.name,
-                 "data" : { "render_data" : self.upload_step.render_data,
-                            "add_enclosing_domain" : self.add_enclosing_domain } }
-    
+                 "data" : { "solid_names" : self.materials,
+                            "names" : self.boundaries,
+                            "meshsize_solids" : self.meshsize_solids,
+                            "meshsize_faces" : self.meshsize_faces } }
+
 
 @register_application
 class MeshingModel(BaseModel):
@@ -149,14 +136,90 @@ class MeshingModel(BaseModel):
         super().__init__(*args, **kwargs)
         self.geo_upload = GeometryUpload(self.model_file.path)
         self.geo_step = GeometryStep(self.geo_upload)
-        self.steps = [self.geo_upload, self.geo_step]
+        self.granularity = SelectionDialog("Granularity",
+                                           ["Default",
+                                            "Very Coarse",
+                                            "Coarse",
+                                            "Moderate",
+                                            "Fine",
+                                            "Very Fine"])
+        self.maxh = FloatParameter("Max Meshsize", default=None)
+        self.grading = FloatParameter("Grading", default=0.3)
+
+        def updateGranularity():
+            if self.granularity.selected == "Very Coarse":
+                self.grading.value = 0.7
+                return
+            if self.granularity.selected == "Coarse":
+                self.grading.value = 0.5
+                return
+            if self.granularity.selected == "Default":
+                self.grading.value = 0.3
+                return
+            if self.granularity.selected == "Moderate":
+                self.grading.value = 0.3
+                return
+            if self.granularity.selected == "Fine":
+                self.grading.value = 0.2
+                return
+            if self.granularity.value == "Very Fine":
+                self.grading.value = 0.1
+                return
+        self.granularity.on_update = updateGranularity
+
+        self.mparam_step = ParameterStep(name="Meshing Parameters",
+                                         parameters=[self.granularity,
+                                                     self.maxh,
+                                                     self.grading])
+        self.steps = [self.geo_upload, self.geo_step, self.mparam_step]
 
     def run(self, result_dir):
-        # save outputs to dir
-        pass
+        from netgen.occ import OCCGeometry
+        from netgen.meshing import MeshingParameters
+        from ngsolve import Mesh
+        geo = OCCGeometry(self.model_file.path("geo.step"))
+        shape = geo.shape
+        mats = self.geo_step.materials
+        maxh = self.geo_step.meshsize_solids
+        for i, s in enumerate(shape.solids):
+            if mats is not None and mats[i] is not None:
+                s.name = mats[i]
+            if maxh is not None and maxh[i] is not None:
+                s.maxh = maxh[i]
+        bnds = self.geo_step.boundaries
+        maxh = self.geo_step.meshsize_faces
+        for i, f in enumerate(shape.faces):
+            if bnds is not None and bnds[i] is not None:
+                f.name = bnds[i]
+            if maxh is not None and maxh[i] is not None:
+                f.maxh = maxh[i]
+        kwargs = {}
+        if self.maxh.value is not None:
+            kwargs["maxh"] = self.maxh.value
+        kwargs["grading"] = self.grading.value
+        geo = OCCGeometry(shape)
+        mesh = geo.GenerateMesh(**kwargs)
+        mesh.Save(os.path.join(result_dir, "mesh.vol.gz"))
+        self.mesh = Mesh(mesh)
+        self.info = { "ne" : self.mesh.ne,
+                      "nse" : self.mesh.nface,
+                      }
+
+    def getReportTemplate(self):
+        ne, nse = self.info["ne"], self.info["nse"]
+        return """
+# Mesh Result
+
+Number of Elements: {ne}
+Number of Surface Elements: {nse}
+
+[[mesh]]
+""".format(ne=ne, nse=nse)
+
+    def RenderObject(self, what):
+        if what == "mesh":
+            return DrawPNG(what, self.mesh)
 
     @staticmethod
     def getDescription():
         return "Create mesh from uploaded geometry to be used in further simulations"
-
-    
