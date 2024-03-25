d557d66750f413c8e7df0610be1e06c4701c3a50
Christopher Lackner <clackner@cerbsim.com>
2024-03-25 21:38:00 +0100

    updates for settings on subshapes

--- meshing_app/mesher.py
+++ meshing_app/mesher.py
@@ -1,30 +1,18 @@
-from webapp_client import (
-    BaseModel,
-    Group,
-    register_application,
-    FileInput,
-    Loading,
-    WebguiComponent,
-    FloatParameter,
-    StringParameter,
-    Switch,
-    Steps,
-    updateFrontendComponents,
-    Button,
-    SelectionDialog
-)
+from webapp_client import *
 from .version import __version__
 
 class ShapeComponent(Group):
     def __init__(self, id, shape, redraw_command, highlight_color, default_color):
-        super().__init__(id=id, flat=False, horizontal=True)
         self.shape = shape
-        self.name = StringParameter(id=id + "_name", name="Name", default=shape.name)
-        self.maxh = FloatParameter(id=id + "_maxh", name="Meshsize", default=shape.maxh)
-        self.visible = Switch(id=id + "_visible", name="Visible", default=True)
+        self.nameParam = StringParameter(id=id + "_name", name="Name",
+                                         default=shape.name)
+        self.maxh = FloatParameter(id=id + "_maxh", name="Meshsize",
+                                   default=shape.maxh)
+        self.visibleParam = Switch(id=id + "_visible", name="Visible", default=True)
+        super().__init__(id=id, flat=False, horizontal=False,
+                         components=[self.nameParam, self.maxh, self.visibleParam])
         self.redraw = redraw_command
         self._selected = False
-        self._visible = True
         self._default_color = default_color
         self._highlight_color = highlight_color
         self._set_color()
@@ -34,7 +22,7 @@ class ShapeComponent(Group):
         return self._selected
 
     def _set_color(self):
-        if not self._visible:
+        if not self.visibleParam.value:
             self._color = (0, 0, 0, 0)
         else:
             if self._selected:
@@ -42,7 +30,7 @@ class ShapeComponent(Group):
             else:
                 self._color = self._default_color
         if self._id.startswith("solids"):
-            if self.visible:
+            if self.visibleParam.value:
                 self.shape.faces.col = self._color
         else:
             self.shape.col = self._color
@@ -55,20 +43,34 @@ class ShapeComponent(Group):
 class ShapeGroup(Group):
     def __init__(
         self,
+        id,
         *args,
         redraw_command=None,
-        default_color=(0, 0, 0),
+        default_color=(0.7, 0.7, 0.7),
         highlight_color=(1, 0, 0),
         **kwargs
     ):
-        super().__init__(*args, **kwargs)
+        self.selector = SelectionDialog(id=id + "_selector", labels=[], values=[],
+                                        variant="dropdown")
+        self.selector.on("update", self.on_shape_select)
         self._redraw_command = redraw_command
         self._default_color = default_color
         self._highlight_color = highlight_color
         self._shapes = []
+        components = [self.selector]
+        super().__init__(id=id, *args, components=components,
+                         inline=False, **kwargs)
+
+    def on_shape_select(self):
+        selected = self.selector.input.value
+        for i, shape in enumerate(self.shape_components):
+            shape.selected = i == selected
+            self.shape_components[i].dynamic.visible = i == selected
+        updateFrontendComponents(self.shape_components)
+        self._redraw_command()
 
     def __getitem__(self, index):
-        return self.components[index]
+        return self.shape_components[index]
 
     @property
     def shapes(self):
@@ -78,7 +80,11 @@ class ShapeGroup(Group):
     def shapes(self, shapes):
         self._shapes = shapes
         self.shape_components = []
+        labels = []
+        values = []
         for i, shape in enumerate(shapes):
+            labels.append(self.name[:-1] + " " + str(i) + ("" if not shape.name else " (" + shape.name + ")"))
+            values.append(i)
             self.shape_components.append(
                 ShapeComponent(
                     id=self._id + "_" + str(i),
@@ -88,7 +94,11 @@ class ShapeGroup(Group):
                     highlight_color=self._highlight_color,
                 )
             )
-        self.components = self.shape_components
+            self.shape_components[-1].dynamic.visible = False
+        self.replace_components([self.selector] + self.shape_components)
+        self.selector.dynamic.labels = labels
+        self.selector.dynamic.values = values
+        self.selector.updateFrontend()
 
 
 @register_application
@@ -103,63 +113,106 @@ class MeshingModel(BaseModel):
         self.geo_upload = FileInput(
             id="geo_file", label="Geometry Upload", extensions="step,stp,brep"
         )
+        self.generate_mesh_button = Button(id="genmesh", label="Create Mesh",
+                                           disabled=True)
+        self.download_mesh_button = Button(id="downloadmesh", label="Download Mesh",
+                                           disabled=True)
+        upload_group = Group(id="upload_group",
+                             inline=True,
+                             components=[self.geo_upload,
+                                         self.generate_mesh_button,
+                                         self.download_mesh_button],
+                             horizontal=True)
         self.geo = None
         self.webgui = WebguiComponent(
-            id="webgui", initial_load=False, enable_sidebar=False
+            id="webgui"
         )
         self.webgui2 = WebguiComponent(
-            id="webgui2", initial_load=False, enable_sidebar=False
+            id="webgui2"
         )
         self.webgui2.dynamic.visible=False
-        self.webgui.on("click", self.on_webgui_click)
-        self.solids = ShapeGroup(id="solids", name="Solids")
-        self.faces = ShapeGroup(
-            id="faces",
-            name="Faces",
-            default_color=(0.7, 0.7, 0.7),
-            redraw_command=self.redraw,
-        )
-        self.edges = ShapeGroup(
-            id="edges",
-            name="Edges",
-            default_color=(0, 0, 0),
-            redraw_command=self.redraw)
-        self.active_step = "solids"
+        self.webgui.on_click = self.on_webgui_click
+
         # self.shape_selector = Steps(steps=[self.solids, self.faces, self.edges])
-        self.shape_selector = SelectionDialog(id="shape_selector",
-                                              labels=["Solids", "Faces", "Edges"],
-                                              value="Faces")
-                                              
-        def update_active_step(val):
-            print("update active step, val = ", val.dynamic.value)
+        self.shapetype_selector = SelectionDialog(id="shapetype_selector",
+                                                  labels=["Solids",
+                                                          "Faces",
+                                                          "Edges"],
+                                                  value="Faces")
+        def select_shape_type():
+            self.solids.dynamic.visible = self.shapetype_selector.input.value == "Solids"
+            self.faces.dynamic.visible = self.shapetype_selector.input.value == "Faces"
+            self.edges.dynamic.visible = self.shapetype_selector.input.value == "Edges"
+            updateFrontendComponents([self.solids, self.faces, self.edges])
 
-        # self.shape_selector.on("update", update_active_step)
+        self.shapetype_selector.on("update", select_shape_type)
+        self.solids = ShapeGroup(id="solid_selector", name="Solids",
+                                 visible=False,
+                                 redraw_command=self.redraw)
+        self.faces = ShapeGroup(id="face_selector", name="Faces",
+                                redraw_command=self.redraw)
+        self.edges = ShapeGroup(id="edge_selector", name="Edges",
+                                visible=False, redraw_command=self.redraw)
 
-        def draw_geo(comp):
-            print("draw geo")
+        self.shape_selector = Group(id="shape_selector",
+                                    components=[self.shapetype_selector,
+                                                self.solids,
+                                                self.faces,
+                                                self.edges])
+
+        def load_geo(redraw=True):
+            if not self.geo_upload.input.name:
+                return
+            import os
+            name, ext = os.path.splitext(self.geo_upload.input.name)
+            name = name.split("/")[-1]
+            self.input.name = name
+            self.update_frontend()
+            with Loading(self.webgui):
+                import netgen.occ as ngocc
+                with self.geo_upload as geofile:
+                    import os
+                    self.geo = ngocc.OCCGeometry(geofile)
+                    self.generate_mesh_button.dynamic.disabled = False
+                    if redraw:
+                        self.redraw()
+
+        def update_geo():
             if not self.geo_upload.input.name:
                 return
             with Loading(self.webgui):
-                import netgen.occ as ngocc
+                load_geo(redraw=False)
+                self.solids.shapes = self.geo.shape.solids
+                self.faces.shapes = self.geo.shape.faces
+                self.edges.shapes = self.geo.shape.edges
+                self.face_neighbours = {face: set() for face in self.geo.shape.faces}
+                self.solid_indices = { solid : i for i, solid in enumerate(self.geo.shape.solids) }
+                for solid in self.solids.shapes:
+                    for face in solid.faces:
+                        self.face_neighbours[face].add(solid)
+                updateFrontendComponents([self.solids, self.faces, self.edges, self.generate_mesh_button])
+                self.redraw()
 
-                with self.geo_upload as geofile:
-                    print("geofile = ", geofile)
-                    self.geo = ngocc.OCCGeometry(geofile)
-                    self.solids.shapes = self.geo.shape.solids
-                    self.faces.shapes = self.geo.shape.faces
-                    self.edges.shapes = self.geo.shape.edges
-                    self.generate_mesh_button.dynamic.disabled = False
-                    updateFrontendComponents([self.solids, self.faces, self.edges, self.generate_mesh_button])
-                    self.redraw()
-
-        self.geo_upload.on("load", draw_geo)
-        self.geo_upload.on("update", draw_geo)
+        self.geo_upload.on("load", load_geo)
+        self.geo_upload.on("update", update_geo)
         def do_nothing(*_):
             updateFrontendComponents([])
+
         self.meshsize = FloatParameter(
             id="meshsize", name="Meshsize", default=None, required=False,
             on_update=do_nothing
         )
+        self.curvature = FloatParameter(
+            id="curvature", name="Curvature Safety", default=None, required=False,
+            on_update=do_nothing
+        )
+
+        global_parameters = Group(id="global_parameters",
+                                  name="Global Parameters",
+                                  title_level=3,
+                                  flat=False,
+                                  components=[self.meshsize,
+                                              self.curvature])
 
         geo_button = Button(id="geobutton", label="Geometry")
         mesh_button = Button(id="meshbutton", label="Mesh")
@@ -172,10 +225,12 @@ class MeshingModel(BaseModel):
 
         webgui_selector = Group(id="webgui_selector", components=[geo_button, mesh_button], horizontal=True)
 
-        self.generate_mesh_button = Button(id="genmesh", label="Create Mesh",
-                                           disabled=True)
-        self.save_mesh_button = Button(id="savemesh", label="Save Mesh",
-                                       disabled=True)
+        def download_mesh(btn):
+            if self.mesh is not None:
+                self.mesh.Save("mesh.vol")
+                btn.callJSFunction("download", { "filename" : "mesh.vol" })
+        self.download_mesh_button.on("click", download_mesh)
+
         def generate_mesh(btn):
             meshing_pars = {}
             with Loading(self.component):
@@ -185,13 +240,11 @@ class MeshingModel(BaseModel):
                 self.webgui.dynamic.visible=False
                 self.webgui2.dynamic.visible=True
                 updateFrontendComponents([self.webgui, self.webgui2])
+                self.download_mesh_button.dynamic.disabled = False
+                self.download_mesh_button.updateFrontend()
                 self.webgui2.draw(self.mesh)
 
         self.generate_mesh_button.on("click", generate_mesh)
-
-        meshing_parameters = Group(id="meshing_parameters",
-                                   components=[self.meshsize,
-                                               self.generate_mesh_button])
 
         webgui_group = Group(id="webgui_group",
                              components=[webgui_selector, self.webgui, self.webgui2])
@@ -200,9 +253,9 @@ class MeshingModel(BaseModel):
                                         webgui_group],
                             horizontal=True)
         self.component = Group(id="main",
-                               components=[self.geo_upload,
+                               components=[upload_group,
                                            horiz_group,
-                                           meshing_parameters])
+                                           global_parameters])
 
     def on_webgui_click(self, args):
         if args["did_move"]:
@@ -210,16 +263,35 @@ class MeshingModel(BaseModel):
         if self.geo is None:
             return
         if not args["ctrlKey"]:
-            for f in self.faces.components:
+            for f in self.faces.shape_components:
                 if f.selected:
                     f.selected = False
-            for e in self.edges.components:
+            for e in self.edges.shape_components:
                 if e.selected:
                     e.selected = False
-        if args["dim"] == 2:
-            self.faces[args["index"]].selected = not self.faces[args["index"]].selected
-        if args["dim"] == 1:
-            self.edges[args["index"]].selected = not self.edges[args["index"]].selected
+        if self.shapetype_selector.input.value == "Faces":
+            if args["dim"] == 2:
+                self.faces.selector.input.value = args["index"]
+                self.faces.selector.updateFrontend()
+                self.faces[args["index"]].selected = not self.faces[args["index"]].selected
+                self.faces.on_shape_select()
+        elif self.shapetype_selector.input.value == "Edges":
+            if args["dim"] == 1:
+                self.edges.selector.input.value = args["index"]
+                self.edges.selector.updateFrontend()
+                self.edges[args["index"]].selected = not self.edges[args["index"]].selected
+        else:
+             if args["dim"] == 2:
+                one = False
+                for solid in self.face_neighbours[self.faces.shapes[args["index"]]]:
+                    if self.solids[self.solid_indices[solid]].visible:
+                        one = not one
+                if one:
+                    index = self.solid_indices[solid]
+                    self.solids.selector.input.value = index
+                    self.solids.selector.updateFrontend()
+                    self.solids[index].selected = \
+                        not self.solids[index].selected
         self.redraw()
 
     def redraw(self):
