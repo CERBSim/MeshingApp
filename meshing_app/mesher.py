306794dca7ac98d5162e2c903b1d2f5daaf52884
Christopher Lackner <clackner@cerbsim.com>
2024-04-16 13:04:29 +0200

    use components from webapp_client

--- meshing_app/mesher.py
+++ meshing_app/mesher.py
@@ -1,138 +1,32 @@
 from webapp_client import *
-from webapp_client.components import Row, Col, Div
+from webapp_client.components import Row, Col, Div, FileUpload, Heading
 from webapp_client.qcomponents import *
 from .version import __version__
 
 btn_style = "margin: 1px; padding: 5px;"
 
-class UserWarning(QDialog):
-    def __init__(self, title, message):
-        card = QCard(
-            children=[QCardSection(children=Div(classes="text-h6",
-                                                style="color: warning;",
-                                                children=title)),
-                      QCardSection(classes="q-pt-none",
-                                   children=message),
-                      QCardActions(align="right",
-                                   children=QBtn(
-                                       flat=True,
-                                       label="Ok",
-                                       color="primary").on_click(self.close))])
-        super().__init__(children=card)
-
-    def close(self):
-        self.model_value = False
-        self.update_frontend()
-
-class FileUpload(QFile):
-    def __init__(self, app, id=None, error_title="Error in File Upload",
-                 error_text="Please upload a valid file", **kwargs):
-        style = kwargs.get("style", { "height": "100px",
-                                      "border": "1px solid rgba(60, 190, 242, 1)",
-                                      "border-radius": "15px",
-                                      "background-color": "rgba(60, 190, 242, .2)",
-                                      "margin-top": "30px",
-                                      "margin-bottom": "70px",
-                                      "padding": "20px",
-                                      "border-style": "dashed",
-                                      "max-width": "400px"})
-        super().__init__(id=id, style=style, **kwargs)
-        self.app = app
-        self.user_warning = UserWarning(title=error_title, message=error_text)
-        self.slot_prepend = [QIcon(name="upload"), self.user_warning]
-        self.on_update_model_value(self.read_file)
-        self.on_clear(self.clear_file)
-        self.filename = None
-        self.file_data = None
-        self.on_rejected(self.user_warning.show)
-        self.on_file_loaded_callbacks = []
-
-    def clear_file(self):
-        self.filename = None
-        self.file_data = None
-
-    def read_file(self):
-        self.filename = self.model_value.name
-        self.model_value.arrayBuffer().then(self.set_file_data)
-
-    def set_file_data(self, arraybuffer):
-        self.file_data = arraybuffer.to_bytes()
-        for callback in self.on_file_loaded_callbacks:
-            callback()
-
-    def dump(self):
-        print(dir(self.model_value))
-        if self.file_data is not None:
-            self.app._store_data_file(self.file_data.decode(), self.filename)
-        return { "filename": self.filename, }
-                 # "filedata": self.file_data }
-
-    def load(self, data):
-        self.filename = data["filename"]
-        if self.filename is not None:
-            self.file_data = self.app._load_data_file(self.filename).encode()
-        print("model value = ", self.model_value)
-
-
-    def __enter__(self) -> list[str] | str:
-        # Create a temporary file storing the data
-        import tempfile, os
-        self.tmpdir = tempfile.TemporaryDirectory()
-        tmp_path = self.tmpdir.__enter__()
-        if isinstance(self.file_data, bytes):
-            file_path = os.path.join(tmp_path, self.filename)
-            with open(file_path, "wb") as file:
-                file.write(self.file_data)
-            return file_path
-
-        file_paths = []
-        data_items = copy.deepcopy(self.data)
-        for file_name, data in data_items.items():
-            if file_name.endswith(".zip"):
-                import base64
-                import io
-                import zipfile
-
-                byte_stream = io.BytesIO(base64.b64decode(data))
-                with zipfile.ZipFile(byte_stream, "r") as zip_ref:
-                    zip_ref.extractall(tmp_path)
-                    extracted_file_names = zip_ref.namelist()
-                    for extracted_file_name in extracted_file_names:
-                        file_path = os.path.join(tmp_path, extracted_file_name)
-                        with open(file_path, "r", encoding="utf-8") as file:
-                            data = file.read()
-                        self.data[extracted_file_name] = data
-                        self._state.name.append(extracted_file_name)
-                        file_paths.append(file_path)
-
-                # Remove the zip file from the data
-                del self._state.data[file_name]
-                self._state.name.remove(file_name)
-            else:
-                file_path = os.path.join(tmp_path, file_name)
-                with open(file_path, "w", encoding="utf-8") as file:
-                    file.write(data)
-                file_paths.append(file_path)
-        return file_paths if len(file_paths) > 1 else file_paths[0]
-
-    def __exit__(self, *args, **kwargs):
-        self.tmpdir.__exit__(*args, **kwargs)
-
-        
 
 class ShapeComponent(QItem):
     def __init__(self, id, shape):
         self.nameParam = QInput(
-            id=id + "_name", label="Name",
+            id=id + "_name",
+            label="Name",
             model_value=shape.name,
             debounce=200,
-            style="min-width: 200px").on_update_model_value(self.set_name)
-        self.maxh = QInput(id=id + "_maxh", label="Meshsize",
-                           type="number",
-                           debounce=200,
-                           model_value=shape.maxh if shape.maxh < 1e98 else None
-                           ).on_update_model_value(self.set_maxh)
-        super().__init__(children=Row([QItemSection(children=c) for c in [self.nameParam, self.maxh]]))
+            style="min-width: 200px",
+        ).on_update_model_value(self.set_name)
+        self.maxh = QInput(
+            id=id + "_maxh",
+            label="Meshsize",
+            type="number",
+            debounce=200,
+            model_value=shape.maxh if shape.maxh < 1e98 else None,
+        ).on_update_model_value(self.set_maxh)
+        super().__init__(
+            children=Row(
+                [QItemSection(children=c) for c in [self.nameParam, self.maxh]]
+            )
+        )
         self._shape = shape
 
     def set_name(self, name):
@@ -140,7 +34,7 @@ class ShapeComponent(QItem):
 
     def set_maxh(self, maxh):
         self._shape.maxh = maxh
-        
+
 
 class ShapeGroup(DynamicGroup):
     def __init__(
@@ -152,9 +46,12 @@ class ShapeGroup(DynamicGroup):
         highlight_color=(1, 0, 0),
         **kwargs
     ):
-        self.selector = SelectionDialog(id=id + "_sel_group", labels=[], values=[],
-                                        variant="dropdown",
-                                        )
+        self.selector = SelectionDialog(
+            id=id + "_sel_group",
+            labels=[],
+            values=[],
+            variant="dropdown",
+        )
         self.selector.dynamic.label = "Select " + kwargs["name"]
         self.selector.dynamic.style = "width: 400px"
         self.selector.on("update", self.on_shape_select)
@@ -163,8 +60,7 @@ class ShapeGroup(DynamicGroup):
         self._highlight_color = highlight_color
         self._shapes = []
         components = [self.selector]
-        super().__init__(id=id, *args, components=components,
-                         inline=True, **kwargs)
+        super().__init__(id=id, *args, components=components, inline=True, **kwargs)
 
     @property
     def shape_components(self):
@@ -193,7 +89,12 @@ class ShapeGroup(DynamicGroup):
         labels = []
         values = []
         for i, shape in enumerate(shapes):
-            labels.append(self.name[:-1] + " " + str(i) + ("" if not shape.name else " (" + shape.name + ")"))
+            labels.append(
+                self.name[:-1]
+                + " "
+                + str(i)
+                + ("" if not shape.name else " (" + shape.name + ")")
+            )
             values.append(i)
             shape_components.append(
                 ShapeComponent(
@@ -209,6 +110,7 @@ class ShapeGroup(DynamicGroup):
         self.selector.dynamic.labels = labels
         self.selector.dynamic.values = values
         self.selector.updateFrontend()
+
 
 @register_application
 class MeshingModel(BaseModel):
@@ -400,19 +302,23 @@ class MeshingModel(BaseModel):
             if args["dim"] == 2:
                 self.faces.selector.input.value = args["index"]
                 self.faces.selector.updateFrontend()
-                self.faces[args["index"]].selected = not self.faces[args["index"]].selected
+                self.faces[args["index"]].selected = not self.faces[
+                    args["index"]
+                ].selected
                 self.faces.on_shape_select()
         elif self.shapetype_selector.input.value == "Edges":
             if args["dim"] == 1:
                 self.edges.selector.input.value = args["index"]
                 self.edges.selector.updateFrontend()
-                self.edges[args["index"]].selected = not self.edges[args["index"]].selected
+                self.edges[args["index"]].selected = not self.edges[
+                    args["index"]
+                ].selected
                 self.edges.on_shape_select()
             elif args["dim"] == 2:
                 face = self.faces.shapes[args["index"]]
                 print("args = ", args)
         else:
-             if args["dim"] == 2:
+            if args["dim"] == 2:
                 one = False
                 for solid in self.face_neighbours[self.faces.shapes[args["index"]]]:
                     if self.solids[self.solid_indices[solid]].visibleParam.value:
@@ -421,8 +327,7 @@ class MeshingModel(BaseModel):
                     index = self.solid_indices[solid]
                     self.solids.selector.input.value = index
                     self.solids.selector.updateFrontend()
-                    self.solids[index].selected = \
-                        not self.solids[index].selected
+                    self.solids[index].selected = not self.solids[index].selected
         self.redraw()
 
     def draw(self):
@@ -440,21 +345,16 @@ class MeshingModel(BaseModel):
         pass
 
     def _update_geometry(self):
-        print("call update geometry")
         with self.geo_upload as geofile:
             import netgen.occ as ngocc
+
             self.geo = ngocc.OCCGeometry(geofile).shape
-            print("still in geofile")
-        print("self.geo = ", self.geo)
         face_components = []
         for i, face in enumerate(self.geo.faces):
-            print("add face", i, face)
-            comp = ShapeComponent("face_" + str(i+1), face)
+            comp = ShapeComponent("face_" + str(i + 1), face)
             face_components.append(comp)
-        print("face children = ", face_components)
         self.faces.children = face_components
         self.faces.update_frontend()
-        print("set dialogs")
         self.geo_upload_dialog.hidden = True
         self.main_dialog.hidden = False
         self.geo_upload_dialog.update_frontend()
@@ -470,27 +370,36 @@ class MeshingModel(BaseModel):
         self.geo_upload_dialog.update_frontend()
 
     def create_geo_upload_layout(self):
-        self.geo_upload = FileUpload(self,
-                                     id="geo_file",
-                                    label="Upload geometry",
-                                    accept="step,stp,brep",
-                                    error_title="Error in Geometry Upload",
-                                    error_text="Please upload a valid geometry file")
+        self.geo_upload = FileUpload(
+            self,
+            id="geo_file",
+            label="Upload geometry",
+            accept="step,stp,brep",
+            error_title="Error in Geometry Upload",
+            error_message="Please upload a valid geometry file",
+        )
         self.geo_upload.on_file_loaded_callbacks.append(self._update_geometry)
-        welcome_header = Div(classes="text-h6",
-                           children="Welcome to the Meshing App!",
-                           style="padding-top:100px;text-align:center;")
-        welcome_text = Div(style="text-align:center;",
-                           children="Upload a geometry file to get started. Currently supported geometry formats: step (*.step, *.stp), brep (*.brep)")
+        welcome_header = Heading(
+            "Welcome to the Meshing App!",
+            6,
+            style="padding-top:100px;text-align:center;",
+        )
+        welcome_text = Div(
+            style="text-align:center;",
+            children="Upload a geometry file to get started. Currently supported geometry formats: step (*.step, *.stp), brep (*.brep)",
+        )
 
         # This is not really nice...
         self.geo_upload_dialog = QCard(
             flat=True,
             classes="q-ma-lg q-pa-lg q-center gt-sm",
-            children=[welcome_header, welcome_text, Div(
-                Col(self.geo_upload),
-                classes="column items-center")])
-    
+            children=[
+                welcome_header,
+                welcome_text,
+                Div(Col(self.geo_upload), classes="column items-center"),
+            ],
+        )
+
     def create_layout(self):
         self.create_geo_upload_layout()
         self.create_main_layout()
@@ -502,40 +411,59 @@ class MeshingModel(BaseModel):
             self._update_geometry()
 
     def create_main_layout(self):
-        self.shapetype_selector = QBtnToggle(model_value="Faces",
-                                             options=["Solids", "Faces", "Edges"])
+        self.shapetype_selector = QBtnToggle(
+            model_value="Faces", options=["Solids", "Faces", "Edges"]
+        )
 
-
-        sim_name = QInput(model_value=self.name,
-                          id="simulation_name",
-                          label="Simulation Name",
-                          debounce=200,
-                          style="padding-left:10px;",
-                          clearable=True)
-        save_button = QBtn(flat=True, icon="save",
-                           children=QTooltip(children="Save simulation",
-                                             style="min-width:400px;"))
+        sim_name = QInput(
+            model_value=self.name,
+            id="simulation_name",
+            label="Simulation Name",
+            debounce=200,
+            style="padding-left:10px;",
+            clearable=True,
+        )
+        save_button = QBtn(
+            flat=True,
+            icon="save",
+            children=QTooltip(children="Save simulation", style="min-width:400px;"),
+        )
         save_button.on_click(self.save)
-        gen_mesh_btn = QBtn(icon="mdi-arrow-right-drop-circle-outline",
-                            flat=True,
-                            children=QTooltip(children="Generate Mesh"))
-        download_mesh_btn = QBtn(icon="download",
-                                 flat=True,
-                                 children=QTooltip(children="Download Mesh"))
-        restart_btn = QBtn(icon="mdi-restart",
-                           flat=True,
-                           children=QTooltip(children="Go back to upload geometry"))
+        gen_mesh_btn = QBtn(
+            icon="mdi-arrow-right-drop-circle-outline",
+            flat=True,
+            children=QTooltip(children="Generate Mesh"),
+        )
+        download_mesh_btn = QBtn(
+            icon="download", flat=True, children=QTooltip(children="Download Mesh")
+        )
+        restart_btn = QBtn(
+            icon="mdi-restart",
+            flat=True,
+            children=QTooltip(children="Go back to upload geometry"),
+        )
         restart_btn.on_click(self.restart)
-        footer = QFooter(classes="row text-black bg-grey-3",
-                         children=[sim_name, QSpace(), restart_btn, save_button,
-                                   gen_mesh_btn, download_mesh_btn])
+        footer = QFooter(
+            classes="row text-black bg-grey-3",
+            children=[
+                sim_name,
+                QSpace(),
+                restart_btn,
+                save_button,
+                gen_mesh_btn,
+                download_mesh_btn,
+            ],
+        )
 
         self.faces = QList()
         inner = Div(self.faces)
 
-        big_device = QCard(flat=True, classes="q-ma-lg q-pa-lg q-center gt-sm",
-                           children=inner,
-                           bordered=True)
+        big_device = QCard(
+            flat=True,
+            classes="q-ma-lg q-pa-lg q-center gt-sm",
+            children=inner,
+            bordered=True,
+        )
         small_device = QCard(flat=True, classes="lt-md", children=inner)
         self.main_dialog = Div([small_device, big_device, footer])
         self.main_dialog.hidden = True
