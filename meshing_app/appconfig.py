3212bd45bf7fe61ad32ec4d0ee53a1160ea10f35
Christopher Lackner <clackner@cerbsim.com>
2024-05-03 10:46:14 +0200

    add meshing app image

--- meshing_app/appconfig.py
+++ meshing_app/appconfig.py
@@ -1,6 +1,8 @@
 from webapp_client import AppConfig, AppAccessConfig
 from .version import __version__
 from .app import MeshingApp
+from webapp_client.utils import load_image
+import os
 
 config = AppConfig(
     name="Meshing App",
@@ -8,6 +10,8 @@ config = AppConfig(
     python_class=MeshingApp,
     frontend_pip_dependencies=["netgen"],
     frontend_dependencies=[],
+    image=load_image(os.path.join(
+        os.path.dirname(__file__),"assets/app_image.png")),
     description="Create a mesh from a STEP geometry file. Assign boundary conditions and mesh size interactively. Download the mesh in Netgen format.",
     compute_environments=[],
     access=AppAccessConfig(),
